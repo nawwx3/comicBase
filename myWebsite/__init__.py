2846068289735e7ada11b60b6eb3c6e7a5d29e01
Nathan Welch <nawwx3@mst.edu>
2018-04-20 01:13:50 -0500

    more pages

--- myWebsite/__init__.py
+++ myWebsite/__init__.py
@@ -3,12 +3,20 @@ from comicBase.comicBase import comicBase_home
 
 app = Flask(__name__)
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
 @app.route('/comicBase')
-def h2():
+def comicBase_home_page():
     return comicBase_home()
+
+@app.route('/about_me')
+def about_me():
+    return render_template('about_me.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 if __name__ == '__main__':
     app.run()
