08e44746535d9a0ba66e437a978473bf5ef2b95d
Nathan Welch <nawwx3@mst.edu>
2018-04-21 22:03:20 -0500

    working flash

--- myWebsite/__init__.py
+++ myWebsite/__init__.py
@@ -1,26 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, flash, redirect, url_for
+
+import os
+
+
 from comicBase.comicBase import cb_home, cb_login
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development_key',
+    USERNAME='f',
+    PASSWORD='f'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/about_me')
 def about_me():
+    flash('all the tests are happening')
     return render_template('mw_about_me.html')
 
 @app.route('/projects')
 def projects():
     return render_template('mw_projects.html')
 
+
+
+
 @app.route('/comicBase')
 def cb_home_page():
+    flash('this is the home page')
     return cb_home()
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def cb_login_page():
-    return cb_login()
+    a = cb_login(app)
+    if a[1] not in [None, '']:
+        flash(a[1])
+    return a[0]
+    # return render_template('cb_login.html')
 
 if __name__ == '__main__':
     app.run()
--- myWebsite/comicBase/comicBase.py
+++ myWebsite/comicBase/comicBase.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import Flask, render_template, session, request, flash, redirect, url_for
 
 def cb_home():
     return render_template('cb_home.html')
 
-def cb_login():
-    return render_template('cb_login.html')
+def cb_login(app):
+    error = None
+    if request.method == 'POST':
+        if request.form['login_username'] !=  app.config['USERNAME']:
+            error = 'Invalid username/password'
+        elif request.form['login_password'] != app.config['PASSWORD']:
+            error = 'Invalid username/password'
+        else:
+            session['logged_in'] = True
+            return (redirect(url_for('cb_home_page')), 'Successfully Logged In')
+    return (render_template('cb_login.html', error=error), '')
