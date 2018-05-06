dee6b971038541a511029fd0ee85c3f7a6c4b17e
Nahtan Welch <nawwx3@mst.edu>
2018-05-06 04:28:11 -0500

    starting to add Rebirth comics

--- myWebsite/init_db.py
+++ myWebsite/init_db.py
@@ -9,15 +9,23 @@ db = sqlite3.connect(helper.database_location)
 
 db.execute('DROP TABLE IF exists comics')
 print('comics table dropped')
-
-db.execute('DROP TABLE if exists wonder_woman_1')
-print('wonder_woman_1 dropped')
-
-db.execute('DROP TABLE if exists superman_2')
-print('superman_2 dropped')
-
-db.execute('DROP TABLE if exists action_comics_1')
-print('action_comics_1 dropped')
+db.execute('DROP TABLE IF exists action_comics_rebirth');
+db.execute('DROP TABLE IF exists batgirl_rebirth');
+db.execute('DROP TABLE IF exists batman_rebirth');
+db.execute('DROP TABLE IF exists green_lanterns_rebirth');
+db.execute('DROP TABLE IF exists supergirl_rebirth');
+db.execute('DROP TABLE IF exists superman_rebirth');
+db.execute('DROP TABLE IF exists the_flash_rebirth');
+db.execute('DROP TABLE IF exists wonder_woman_rebirth');
+db.execute('DROP TABLE IF exists detective_comics_rebirth');
+db.execute('DROP TABLE IF exists nightwing_rebirth');
+db.execute('DROP TABLE IF exists teen_titans_rebirth');
+db.execute('DROP TABLE IF exists trinity_rebirth');
+db.execute('DROP TABLE IF exists deathstroke_rebirth');
+db.execute('DROP TABLE IF exists super_sons_rebirth');
+db.execute('DROP TABLE IF exists doomsday_clock_rebirth');
+db.execute('DROP TABLE IF exists superwoman_rebirth');
+db.execute('DROP TABLE IF exists titans_rebirth');
 
 
 
@@ -31,6 +39,11 @@ with app.open_resource('sql/entries.sql', mode='r') as f:
     db.cursor().executescript(f.read())
 db.commit()
 print('Entries initialized')
+
+with app.open_resource('sql/other.sql', mode='r') as f:
+    db.cursor().executescript(f.read())
+db.commit()
+print('Others initialized')
 
 print("Database initialized!")
 
--- myWebsite/sql/entries.sql
+++ myWebsite/sql/entries.sql
@@ -1,12 +1,137 @@
-insert into comics values("1", "wonder_woman_1");
-insert into comics values("2", "action_comics_1");
-insert into comics values("3", "superman_2");
+insert into comics values("1", "action_comics_rebirth");
+CREATE TABLE if not exists action_comics_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
 
-insert into wonder_woman_1 values ("1", "12", "Title", "Arc", "1.20");
-insert into wonder_woman_1 values ("2", "15", "other title", "other arc", "1.20");
-
-insert into action_comics_1 values ("1", "12", "Title", "Arc", "1.20");
-insert into action_comics_1 values ("2", "15", "other title", "other arc", "1.20");
-
-insert into superman_2 values ("1", "12", "Title", "Arc", "1.20");
-insert into superman_2 values ("2", "2", "other title", "other arc", "1.20");
+insert into comics values("2", "batgirl_rebirth");
+CREATE TABLE if not exists batgirl_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("3", "batman_rebirth");
+CREATE TABLE if not exists batman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("6", "green_lanterns_rebirth");
+CREATE TABLE if not exists green_lanterns_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("7", "supergirl_rebirth");
+CREATE TABLE if not exists supergirl_rebirth(
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("8", "superman_rebirth");
+CREATE TABLE if not exists superman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("9", "the_flash_rebirth");
+CREATE TABLE if not exists the_flash_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("10", "wonder_woman_rebirth");
+CREATE TABLE if not exists wonder_woman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("11", "detective_comics_rebirth");
+CREATE TABLE if not exists detective_comics_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("12", "nightwing_rebirth");
+CREATE TABLE if not exists nightwing_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("13", "teen_titans_rebirth");
+CREATE TABLE if not exists teen_titans_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("14", "trinity_rebirth");
+CREATE TABLE if not exists trinity_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("15", "deathstroke_rebirth");
+CREATE TABLE if not exists deathstroke_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("16", "super_sons_rebirth");
+CREATE TABLE if not exists super_sons_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("17", "doomsday_clock_rebirth");
+CREATE TABLE if not exists doomsday_clock_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("18", "superwoman_rebirth");
+CREATE TABLE if not exists superwoman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+insert into comics values("19", "titans_rebirth");
+CREATE TABLE if not exists titans_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
