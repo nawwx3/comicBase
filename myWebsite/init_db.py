810a4afe1300c6485aa3195a11f87413a70f0855
Nahtan Welch <nawwx3@mst.edu>
2018-05-06 16:03:57 -0500

    changed database initialization

--- myWebsite/init_db.py
+++ myWebsite/init_db.py
@@ -4,46 +4,31 @@ import comicBase.helper as helper
 
 app = Flask(__name__)
 
-
 db = sqlite3.connect(helper.database_location)
+cur = db.cursor()
 
-db.execute('DROP TABLE IF exists comics')
+cur.execute(''' SELECT titles FROM comics ''')
+table_titles = cur.fetchall()
+
+for table in table_titles:
+    query = 'DROP TABLE IF exists {}'.format(table[0])
+    print(query)
+    cur.execute(query);
+print('Other existing tables dropped')
+
+cur.execute('DROP TABLE IF exists comics')
 print('comics table dropped')
-db.execute('DROP TABLE IF exists action_comics_rebirth');
-db.execute('DROP TABLE IF exists batgirl_rebirth');
-db.execute('DROP TABLE IF exists batman_rebirth');
-db.execute('DROP TABLE IF exists green_lanterns_rebirth');
-db.execute('DROP TABLE IF exists supergirl_rebirth');
-db.execute('DROP TABLE IF exists superman_rebirth');
-db.execute('DROP TABLE IF exists the_flash_rebirth');
-db.execute('DROP TABLE IF exists wonder_woman_rebirth');
-db.execute('DROP TABLE IF exists detective_comics_rebirth');
-db.execute('DROP TABLE IF exists nightwing_rebirth');
-db.execute('DROP TABLE IF exists teen_titans_rebirth');
-db.execute('DROP TABLE IF exists trinity_rebirth');
-db.execute('DROP TABLE IF exists deathstroke_rebirth');
-db.execute('DROP TABLE IF exists super_sons_rebirth');
-db.execute('DROP TABLE IF exists doomsday_clock_rebirth');
-db.execute('DROP TABLE IF exists superwoman_rebirth');
-db.execute('DROP TABLE IF exists titans_rebirth');
-
-
 
 # inserts the tables into the database
 with app.open_resource('sql/tables.sql', mode='r') as f:
-    db.cursor().executescript(f.read())
+    cur.executescript(f.read())
 db.commit()
 print('Tables initialized')
 
 with app.open_resource('sql/entries.sql', mode='r') as f:
-    db.cursor().executescript(f.read())
+    cur.executescript(f.read())
 db.commit()
 print('Entries initialized')
-
-with app.open_resource('sql/other.sql', mode='r') as f:
-    db.cursor().executescript(f.read())
-db.commit()
-print('Others initialized')
 
 print("Database initialized!")
 
--- myWebsite/sql/tables.sql
+++ myWebsite/sql/tables.sql
@@ -3,3 +3,124 @@ create table comics (
   id integer primary key autoincrement,
   titles varchar(50) not null unique
 );
+
+CREATE TABLE if not exists action_comics_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+
+CREATE TABLE if not exists batgirl_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists batman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists green_lanterns_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists supergirl_rebirth(
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists superman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists the_flash_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists wonder_woman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists detective_comics_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists nightwing_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists teen_titans_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists trinity_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists deathstroke_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists super_sons_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists doomsday_clock_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists superwoman_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
+CREATE TABLE if not exists titans_rebirth (
+    id integer primary key autoincrement,
+    issue_number integer not null,
+    title varchar(100),
+    arc varchar(50),
+    price float
+);
